bb00e4f2557599d949b2d3e8fb6aa4e41bf1a4e5
Yoiner Duran <duranyoiner86@gmail.com>
2023-07-12 16:59:02 -0500

    Inicio maquetado

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+from apps.inventario.views import InicioView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',InicioView.as_view(),name="inicio"),
     path('user/',include('apps.user.urls')),
     path('inventario/',include('apps.inventario.urls')),
 ]
