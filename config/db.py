8cfa6446225eb5284f413d669cf559afa368fb2a
Yoiner Duran <duranyoiner86@gmail.com>
2023-07-12 14:49:19 -0500

    proyecto creado

---
+++ config/db.py
@@ -0,0 +1,10 @@
+MYSQL = {
+  'default':{
+  'ENGINE':'django.db.backends.mysql',
+  'NAME':'inventariosala', #nombre de la base de datos
+  'USER':'root',
+  'PASSWORD':'root',
+  'HOST':'127.0.0.1', #servidor local o también puede ser 'localhost'
+  'PORT':'3306'
+  }
+}
