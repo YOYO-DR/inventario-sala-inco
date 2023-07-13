ce1301f173958dbf9aabd27f21afd26ef51d3614
Yoiner Duran <duranyoiner@gmail.com>
2023-07-13 01:59:05 -0500

    modificando clase de usuario

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+from config.settings import STATIC_URL
+
+class User(AbstractUser):
+    #agrego/modifico este atributo
+    email = models.EmailField(max_length=200,unique=True,null=False,blank=False)
+    #agrego una imagen de perfil
+    image=models.ImageField(upload_to='user_images/%Y/%m/%d/')
+    #le digo que atributo se utilizara para la autenticacion
+    USERNAME_FIELD='email'
+    #los campos requeridos para  un registro y no debe tener el USERNAME_FIELD (el email)
+    REQUIRED_FIELDS = ['username','first_name','last_name']
+
+    def get_image(self):
+        if self.image:
+            return self.image.url
+        return f'{STATIC_URL}media/img/empty_user.png'
--- config/settings.py
+++ config/settings.py
@@ -124,3 +124,6 @@ STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#config usuario
+AUTH_USER_MODEL = 'user.User'
